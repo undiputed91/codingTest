41cc81da34e6eecfd1421ee73af9ac9c3840cfd0
undiputed91 <shin2896@naver.com>
2024-07-16 10:31:42 +0900

    bufferedReader

---
+++ src/main/java/org/example/baekjoon/BufferReader.java
@@ -0,0 +1,17 @@
+package org.example.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BufferReader {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+
+        int a = Integer.parseInt(stringTokenizer.nextToken());
+        System.out.println(a);
+    }
+}
