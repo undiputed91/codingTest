8db7f7167d16ffdafecb3c1cc50eb4ed21817693
undiputed91 <shin2896@naver.com>
2024-09-24 17:43:58 +0900

    백준 1008 a/b

--- src/main/java/org/example/baekjoon/baek1000/Main.java
+++ src/main/java/org/example/baekjoon/baek1000/Main.java
@@ -3,7 +3,7 @@ package org.example.baekjoon.baek1000;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String args[]){
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int A = sc.nextInt();
         int B = sc.nextInt();
