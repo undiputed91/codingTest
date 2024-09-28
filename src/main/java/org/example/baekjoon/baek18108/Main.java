060ecfdb788551949c110b7bcd7389ce9fdc4c0c
undiputed91 <shin2896@naver.com>
2024-09-28 09:18:04 +0900

    백준 18108 1998년생인 내가 태국에서는 2541년생?!

---
+++ src/main/java/org/example/baekjoon/baek18108/Main.java
@@ -0,0 +1,11 @@
+package org.example.baekjoon.baek18108;
+
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int buddaYear = sc.nextInt();
+        System.out.println(buddaYear- 543);
+    }
+}
