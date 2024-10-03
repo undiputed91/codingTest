a149198bff8f27055d0ca669743e9b9fba3dda58
undiputed91 <shin2896@naver.com>
2024-10-03 18:25:02 +0900

    백준 10171 고양이

---
+++ src/main/java/org/example/baekjoon/baek10171/Main.java
@@ -0,0 +1,11 @@
+package org.example.baekjoon.baek10171;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
+// \ 백슬래쉬 입력시 \\-> \로 인식
