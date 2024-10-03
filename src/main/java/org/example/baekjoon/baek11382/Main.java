e12d577c6e84c9cb09c291ddbd1c8e6d990ca652
undiputed91 <shin2896@naver.com>
2024-10-03 18:17:20 +0900

    백준 11382 꼬마 정민

---
+++ src/main/java/org/example/baekjoon/baek11382/Main.java
@@ -0,0 +1,15 @@
+package org.example.baekjoon.baek11382;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        Long A = sc.nextLong();
+        Long B = sc.nextLong();
+        Long C = sc.nextLong();
+
+        System.out.println(A+B+C);
+    }
+}
