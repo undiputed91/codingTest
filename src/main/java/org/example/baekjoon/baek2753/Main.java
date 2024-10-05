aa683b6a51a66f75fbc36f548b12cac5e23f4e43
undiputed91 <shin2896@naver.com>
2024-10-06 06:39:41 +0900

    백준 2753 윤년

---
+++ src/main/java/org/example/baekjoon/baek2753/Main.java
@@ -0,0 +1,17 @@
+package org.example.baekjoon.baek2753;
+
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+
+        if(year%4==0 && (!(year%100==0) || year%400==0)){
+            System.out.println(1);
+        }else{
+            System.out.println(0);
+        }
+
+    }
+}
