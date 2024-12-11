4aef123812573eb653cf81b73e23e1932ea8f92d
undiputed91 <shin2896@naver.com>
2024-12-12 07:41:59 +0900

    프로그래머스 level.0 원하는문자열찾 수정

--- src/main/java/org/example/programmers/level0/원하는문자열찾기/Solution.java
+++ src/main/java/org/example/programmers/level0/원하는문자열찾기/Solution.java
@@ -3,21 +3,11 @@ package org.example.programmers.level0.원하는문자열찾기;
 public class Solution {
     public int solution(String myString, String pat) {
         int answer = 0;
-        char[] charArr = myString.toCharArray();
-        char[] patArr = pat.toCharArray();
-        if(myString.length()>pat.length()){
-            for(int i =0; i<charArr.length; i++){
-                charArr[i] = Character.toUpperCase(charArr[i]);
-            }
-            for(int i =0; i<patArr.length; i++){
-                patArr[i] = Character.toUpperCase(patArr[i]);
-            }
-            String myStr = new String(charArr);
-            String patStr = new String(patArr);
-            if(myStr.contains(patStr)){
-                answer = 1;
-            }
-        }
+        String myStr = myString.toUpperCase();
+        String patStr = pat.toUpperCase();
+        answer = myStr.contains(patStr)? 1 : 0;
         return answer;
     }
 }
+
+// Character.toUpperCase()만 있는게 아니라 String.toUpperCase()도 존재
